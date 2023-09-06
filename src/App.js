e028ae6194affe0d36e1797d12bb0cac6f676295
Eyuel <eyuelworku786@gmail.com>
2023-09-06 12:42:08 +0300

    Added finishing touches

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ import { getIconPath } from "./utils/getIconPath";
 import classes from "./App.module.css";
 import ErrorMessage from "./ErrorMessge";
 
-
 function App() {
-  const cityInputRef = useRef(null)
+  const cityInputRef = useRef(null);
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -19,7 +18,7 @@ function App() {
 
   const handleCloseErrorMessage = () => setError(null);
 
-  const fetchWeatherData = useCallback(async city => {
+  const fetchWeatherData = useCallback(async (city) => {
     try {
       setError(null);
       setLoading(true);
@@ -29,18 +28,18 @@ function App() {
         url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
         params: {
           q: city,
-          days: "3"
+          days: "3",
         },
         headers: {
-          "X-RapidAPI-Key": "1a708579c0mshac2ece0f0172df0p1a6dbcjsn807319ab4da1",
-          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-        }
+          "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+        },
       });
 
       const transformedWeatherData = {
         location: {
           name: data.location.name,
-          country: data.location.country
+          country: data.location.country,
         },
         current: {
           code: data.current.condition.code,
@@ -50,20 +49,19 @@ function App() {
           iconPath: getIconPath(
             data.current.condition.code,
             data.current.is_day
-          )
+          ),
         },
-        forecast: data.forecast.forecastday.map(el => ({
+        forecast: data.forecast.forecastday.map((el) => ({
           code: el.day.condition.code,
           iconPath: getIconPath(el.day.condition.code),
           date: el.date,
           weekDay: getDay(el.date),
           avgTemp: el.day.avgtemp_c,
-          condition: el.day.condition.text
-        }))
+          condition: el.day.condition.text,
+        })),
       };
 
       setWeatherData(transformedWeatherData);
-
     } catch (err) {
       setError("Please enter a valid city name");
     } finally {
@@ -85,7 +83,7 @@ function App() {
     }
   }, [error]);
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const city = cityInputRef.current.value;
@@ -94,7 +92,7 @@ function App() {
     }
 
     fetchWeatherData(city);
-  }
+  };
   // Add Error snackbar
   return (
     <div className={classes.App}>
@@ -105,17 +103,18 @@ function App() {
           id="city"
           name="city"
           placeholder="Enter city here..."
-          ref={cityInputRef} />
+          ref={cityInputRef}
+        />
       </form>
-      {loading &&
+      {loading && (
         <div className={classes.spinnerContainer}>
           <div className={classes.spinner} />
-        </div>}
-      {!loading && error &&
-        <ErrorMessage
-          errorText={error}
-          onClose={handleCloseErrorMessage} />}
-      {!loading && weatherData &&
+        </div>
+      )}
+      {!loading && error && (
+        <ErrorMessage errorText={error} onClose={handleCloseErrorMessage} />
+      )}
+      {!loading && weatherData && (
         <div className={classes.myCard}>
           <CurrentWeather
             city={weatherData.location.name}
@@ -123,7 +122,8 @@ function App() {
             condition={weatherData.current.condition}
             iconPath={weatherData.current.iconPath}
             altText={weatherData.current.condition}
-            isDay={weatherData.current.isDay} />
+            isDay={weatherData.current.isDay}
+          />
           <div className={classes.weekdaysContainer}>
             {weatherData.forecast.map((el, index) => (
               <ForecastDay
@@ -132,10 +132,12 @@ function App() {
                 avgTemp={el.avgTemp}
                 iconPath={el.iconPath}
                 condition={el.condition}
-                altText={el.condition} />
+                altText={el.condition}
+              />
             ))}
           </div>
-        </div>}
+        </div>
+      )}
     </div>
   );
 }
--- src/utils/getDay.js
+++ src/utils/getDay.js
@@ -1,13 +1,22 @@
 export function getDay(dateString) {
-    const dayIndex = new Date(dateString).getDay();
+  const dayIndex = new Date(dateString).getDay();
 
-    switch (dayIndex) {
-        case 0: return "Sunday";
-        case 1: return "Monday";
-        case 2: return "Tuesday";
-        case 3: return "Wednesday";
-        case 4: return "Thursday";
-        case 5: return "Friday";
-        case 6: return "Saturday";
-    }
+  switch (dayIndex) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      return "";
+  }
 }
